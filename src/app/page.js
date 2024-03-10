8ddda353e2f204e290b0f38b5abcc9cd74cc5628
Gr34sy <slupek3@poczta.fm>
2024-03-10 17:56:31 +0100

    changed homepage styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "../styles/scss/pages/homepage.module.scss";
 
 export default function Home() {
@@ -9,7 +8,12 @@ export default function Home() {
       </div>
 
       <div className={styles.homepage__about}>
-        Me
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatem corporis, similique officia repellendus id esse et perspiciatis, dignissimos animi rem, blanditiis ad nostrum. Rerum voluptatibus sint quas, provident cumque accusamus cum repellat rem excepturi sit? Obcaecati repudiandae aperiam rem, eligendi adipisci et error vel neque, amet, tempora sapiente nemo quibusdam nisi ratione aut? Voluptate hic repellat cum recusandae ut fugiat qui dolorum eum molestiae veritatis soluta sequi adipisci quasi voluptatibus aliquid aut, quisquam accusantium ullam corporis repudiandae? Reprehenderit distinctio aspernatur consequuntur harum possimus alias debitis! Velit soluta officiis id ea, sunt molestias assumenda itaque recusandae nobis laboriosam officia molestiae, facilis aspernatur similique cum perspiciatis at sit consequatur? Assumenda natus vitae earum accusamus aspernatur autem beatae, fugit recusandae ipsam blanditiis ratione esse quis ut minus delectus, nemo culpa. Non nam eaque, atque perferendis, ipsum vitae dolore mollitia enim sed expedita eligendi ab commodi quam, suscipit quis beatae facere consequatur laboriosam.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatem corporis, similique officia repellendus id esse et perspiciatis, dignissimos animi rem, blanditiis ad nostrum. Rerum voluptatibus sint quas, provident cumque accusamus cum repellat rem excepturi sit? Obcaecati repudiandae aperiam rem, eligendi adipisci et error vel neque, amet, tempora sapiente nemo quibusdam nisi ratione aut? Voluptate hic repellat cum recusandae ut fugiat qui dolorum eum molestiae veritatis soluta sequi adipisci quasi voluptatibus aliquid aut, quisquam accusantium ullam corporis repudiandae? Reprehenderit distinctio aspernatur consequuntur harum possimus alias debitis! Velit soluta officiis id ea, sunt molestias assumenda itaque recusandae nobis laboriosam officia molestiae, facilis aspernatur similique cum perspiciatis at sit consequatur? Assumenda natus vitae earum accusamus aspernatur autem beatae, fugit recusandae ipsam blanditiis ratione esse quis ut minus delectus, nemo culpa. Non nam eaque, atque perferendis, ipsum vitae dolore mollitia enim sed expedita eligendi ab commodi quam, suscipit quis beatae facere consequatur laboriosam.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatem corporis, similique officia repellendus id esse et perspiciatis, dignissimos animi rem, blanditiis ad nostrum. Rerum voluptatibus sint quas, provident cumque accusamus cum repellat rem excepturi sit? Obcaecati repudiandae aperiam rem, eligendi adipisci et error vel neque, amet, tempora sapiente nemo quibusdam nisi ratione aut? Voluptate hic repellat cum recusandae ut fugiat qui dolorum eum molestiae veritatis soluta sequi adipisci quasi voluptatibus aliquid aut, quisquam accusantium ullam corporis repudiandae? Reprehenderit distinctio aspernatur consequuntur harum possimus alias debitis! Velit soluta officiis id ea, sunt molestias assumenda itaque recusandae nobis laboriosam officia molestiae, facilis aspernatur similique cum perspiciatis at sit consequatur? Assumenda natus vitae earum accusamus aspernatur autem beatae, fugit recusandae ipsam blanditiis ratione esse quis ut minus delectus, nemo culpa. Non nam eaque, atque perferendis, ipsum vitae dolore mollitia enim sed expedita eligendi ab commodi quam, suscipit quis beatae facere consequatur laboriosam.
+
       </div>
     </main>
   );
