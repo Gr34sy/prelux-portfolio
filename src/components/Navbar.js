be545034ca16de2d7272a6768c373f4917e8d277
Gr34sy <slupek3@poczta.fm>
2024-03-11 12:12:09 +0100

    changed decoration in header size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
+//styles
+import styles from '../styles/scss/components/navbar.module.scss';
+
+//images
 import logo from '../assets/images/logo.png';
+
+//components
+import Image from 'next/image';
 
 export function Navbar(){
     return (
-        <nav>
-            <img src={logo} />
+        <nav className={styles.navbar}>
+            <Image src={logo} width={60} height={60} alt="Prelux logo"/>
         </nav>
     )
 }
