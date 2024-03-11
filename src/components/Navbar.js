51409fe3d422add77ca613a184999279e90f52ef
Gr34sy <slupek3@poczta.fm>
2024-03-11 12:44:57 +0100

    added links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
 //styles
-import styles from '../styles/scss/components/navbar.module.scss';
+import styles from "../styles/scss/components/navbar.module.scss";
 
 //images
-import logo from '../assets/images/logo.png';
+import logo from "../assets/images/logo.png";
 
 //components
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
-export function Navbar(){
-    return (
-        <nav className={styles.navbar}>
-            <Image src={logo} width={60} height={60} alt="Prelux logo"/>
-        </nav>
-    )
+export function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.navbar__logo}>
+        <Image src={logo} width={60} height={60} alt="Prelux logo" />
+        <p>Jerzy Legierski</p>
+      </Link>
+
+      <ul className={styles.navbar__list}>
+        <li>
+          <Link href="/portfolio" className={styles.navbar__list_link}>Portfolio</Link>
+        </li>
+
+        <li>
+          <Link href="/contact" className={styles.navbar__list_link}>Contact</Link>
+        </li>
+
+        <li className={styles.navbar__list_icons}>
+          <Link href="/" className={styles.navbar__list_icon}></Link>
+          <Link href="/" className={styles.navbar__list_icon}></Link>
+          <Link href="/" className={styles.navbar__list_icon}></Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
