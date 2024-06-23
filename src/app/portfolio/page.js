6db87e552a89adde6f8c7f3c3af318bf16dda8e7
Gr34sy <piotr.slupski17@gmail.com>
2024-06-23 13:50:52 +0200

    setup the gallery grid

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -3,25 +3,28 @@ import GalleryItem from "@/components/GalleryItem/GalleryItem";
 
 // images
 import sutryk from '../../assets/images/sutryk.jpg';
+import rybka from  '../../assets/images/rybka.jpg';
+import polanski from '../../assets/images/polanski.png';
 
 export default function Portfolio(){
     return (
         <main>
            <Gallery>
                 <GalleryItem image={sutryk} size={2}/>
+                <GalleryItem image={rybka} size={1}/>
+                <GalleryItem image={polanski} size={2}/>
                 <GalleryItem image={sutryk} size={1}/>
+                <GalleryItem image={polanski} size={2}/>
+                <GalleryItem image={rybka} size={1}/>
+                <GalleryItem image={polanski} size={1}/>
+                <GalleryItem image={rybka} size={1}/>
                 <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
+                <GalleryItem image={rybka} size={1}/>
                 <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
                 <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
+                <GalleryItem image={polanski} size={2}/>
                 <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
-                <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
-                <GalleryItem image={sutryk} size={2}/>
-                <GalleryItem image={sutryk} size={1}/>
+                <GalleryItem image={rybka} size={1}/>
            </Gallery>
         </main>
     )
