ab12d32cc15431a6020edf60482b62b0798f8b9b
Piotr Słupski <piotr.slupski17@gmail.com>
2024-08-07 15:59:22 +0200

    added artworks to gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,46 +3,95 @@ import styles from "./gallery.module.scss";
 // hooks
 import { useState } from "react";
 // images
-import sutryk from "../../assets/images/sutryk.jpg";
-import rybka from "../../assets/images/rybka.jpg";
-import polanski from "../../assets/images/polanski.png";
-// components 
+import jeden from "../../assets/artworks/1.png";
+import dwaRokJerzy from "../../assets/artworks/2rokJerzy.png";
+import trzyRefki from "../../assets/artworks/3refki.png";
+import czteryTrojki from "../../assets/artworks/3333.png";
+import bagnoType from "../../assets/artworks/bagnoType.png";
+import choinka from "../../assets/artworks/choinka.png";
+import creepy from "../../assets/artworks/creepy.png";
+import ddfd from "../../assets/artworks/ddfd.png";
+import enviro from "../../assets/artworks/enviro.png";
+import final from "../../assets/artworks/final.png";
+import interior from "../../assets/artworks/interior.png";
+import jadziaFinal from "../../assets/artworks/jadziaFinal.png";
+import kozaczek from "../../assets/artworks/kozaczek.png";
+import lesGo from "../../assets/artworks/lesGo.png";
+import leszy from "../../assets/artworks/leszy.png";
+import nutKing from "../../assets/artworks/nutKing.png";
+import okej from "../../assets/artworks/okej.png";
+import postacMikroorganizmy from "../../assets/artworks/postacMikroorganizmy.png";
+import potwor2 from "../../assets/artworks/potwor2.png";
+import ptak from "../../assets/artworks/ptak.png";
+import robotykonn from "../../assets/artworks/robotykonn.png";
+import samaLama from "../../assets/artworks/samaLama.png";
+import soGood from "../../assets/artworks/soGood.png";
+import spadam from "../../assets/artworks/spadam.png";
+import splashart1 from "../../assets/artworks/splashart1.png";
+import szamann from "../../assets/artworks/szamann.png";
+import szukers from "../../assets/artworks/szukers.png";
+import nacukrowany from "../../assets/artworks/nacukrowany.png";
+import zvz from "../../assets/artworks/zvz.png";
+import zycie from "../../assets/artworks/zycie.png";
+
+// components
 import GalleryItem from "@/components/GalleryItem/GalleryItem";
 import Overlay from "@/components/Overlay/Overlay";
 
 const Gallery = () => {
   const [overlayImages, setOverlayImages] = useState([]);
 
-  function showOverlay(images){
+  function showOverlay(images) {
     setOverlayImages([...images]);
   }
-  function hideOverlay(){
+  function hideOverlay() {
     setOverlayImages([]);
   }
-
-
 
   return (
     <div className={styles.gallery}>
       <h1 className={styles.title}>My artworks</h1>
       <div className={styles.grid}>
-        <GalleryItem image={sutryk} size={2} action={showOverlay} />
-        <GalleryItem image={rybka} size={1} action={showOverlay} />
-        <GalleryItem image={polanski} size={2} action={showOverlay} />
-        <GalleryItem image={sutryk} size={1} action={showOverlay} />
-        <GalleryItem image={polanski} size={2} action={showOverlay} />
-        <GalleryItem image={rybka} size={1} action={showOverlay} />
-        <GalleryItem image={polanski} size={1} action={showOverlay} />
-        <GalleryItem image={rybka} size={1} action={showOverlay} />
-        <GalleryItem image={sutryk} size={2} action={showOverlay} />
-        <GalleryItem image={rybka} size={1} action={showOverlay} />
-        <GalleryItem image={sutryk} size={2} action={showOverlay} />
-        <GalleryItem image={sutryk} size={2} action={showOverlay} />
-        <GalleryItem image={polanski} size={2} action={showOverlay} />
-        <GalleryItem image={sutryk} size={2} action={showOverlay} />
-        <GalleryItem image={rybka} size={1} action={showOverlay} />
+        <GalleryItem image={jeden} size={1} action={showOverlay} />
+        <GalleryItem image={dwaRokJerzy} size={1} action={showOverlay} />
+        <GalleryItem image={trzyRefki} size={1} action={showOverlay} />
+        <GalleryItem image={czteryTrojki} size={2} action={showOverlay} />
+
+        <GalleryItem image={creepy} size={1} action={showOverlay} />
+        <GalleryItem image={ddfd} size={1} action={showOverlay} />
+        <GalleryItem image={enviro} size={2} action={showOverlay} />
+        <GalleryItem image={final} size={2} action={showOverlay} />
+
+        <GalleryItem image={interior} size={2} action={showOverlay} />
+        <GalleryItem image={jadziaFinal} size={1} action={showOverlay} />
+        <GalleryItem image={kozaczek} size={2} action={showOverlay} />
+        <GalleryItem image={lesGo} size={1} action={showOverlay} />
+
+        <GalleryItem image={leszy} size={1} action={showOverlay} />
+        <GalleryItem image={nutKing} size={2} action={showOverlay} />
+        <GalleryItem image={okej} size={1} action={showOverlay} />
+        <GalleryItem
+          image={postacMikroorganizmy}
+          size={1}
+          action={showOverlay}
+        />
+
+        <GalleryItem image={potwor2} size={2} action={showOverlay} />
+        <GalleryItem image={ptak} size={1} action={showOverlay} />
+        <GalleryItem image={robotykonn} size={1} action={showOverlay} />
+        <GalleryItem image={samaLama} size={1} action={showOverlay} />
+
+        <GalleryItem image={soGood} size={1} action={showOverlay} />
+        <GalleryItem image={spadam} size={1} action={showOverlay} />
+        <GalleryItem image={splashart1} size={2} action={showOverlay} />
+        <GalleryItem image={szamann} size={1} action={showOverlay} />
+
+        <GalleryItem image={szukers} size={2} action={showOverlay} />
+        <GalleryItem image={nacukrowany} size={2} action={showOverlay} />
+        <GalleryItem image={zvz} size={1} action={showOverlay} />
+        <GalleryItem image={zycie} size={1} action={showOverlay} />
       </div>
-      <Overlay images={overlayImages} hide={hideOverlay}/>
+      <Overlay images={overlayImages} hide={hideOverlay} />
     </div>
   );
 };
--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import styles from './gallery-item.module.scss';
-import Image from 'next/image';
+import styles from "./gallery-item.module.scss";
+import Image from "next/image";
 
-
-const GalleryItem = ({image, images, size, action}) => {
-
-  function handleClick(){
-    if(typeof action === 'function'){
+const GalleryItem = ({ image, images, size, action }) => {
+  function handleClick() {
+    if (typeof action === "function") {
       action([image]);
     }
   }
 
   return (
-    <div className={`${styles.item} ${styles[`s-${size}`]}`} onClick={handleClick}>
-      <Image src={image} alt="Art" fill/>
-      <div className={styles['top-left']}/>
-      <div className={styles['top-right']}/>
-      <div className={styles['bottom-right']}/>
-      <div className={styles['bottom-left']}/>
+    <div
+      className={`${styles.item} ${styles[`s-${size}`]}`}
+      onClick={handleClick}
+    >
+      <Image src={image} alt="Art" fill />
+      <div className={styles["top-left"]} />
+      <div className={styles["top-right"]} />
+      <div className={styles["bottom-right"]} />
+      <div className={styles["bottom-left"]} />
     </div>
-  )
-}
+  );
+};
 
-export default GalleryItem
+export default GalleryItem;
